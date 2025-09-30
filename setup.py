65c43072fcf1187a35a6fb9f280fdb95ed3dc7b1
emacosta95 <emanuelecosta95@hotmail.com>
2025-09-30 15:28:31 +0200

    add init better

--- setup.py
+++ setup.py
@@ -2,7 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name="QuantumMachineLearningFermions",
-    version="0.1",
+    version="0.1.0",
+    description="Quantum Machine Learning for Fermions",
+    author="Emanuele Costa",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    install_requires=[
+        # list your dependencies here
+        "numpy",
+        "scipy",
+        "matplotlib",
+    ],
+    python_requires=">=3.8",
 )
