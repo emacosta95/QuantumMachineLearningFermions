67203cf838147b1d41e9a556c63247a826e14e59
emacosta95 <emanuelecosta95@hotmail.com>
2025-09-30 14:33:15 +0200

    add init better

---
+++ src/__init__.py
@@ -0,0 +1,21 @@
+from .hamiltonian_utils import *
+from .cg_utils import *
+from .fermi_hubbard_library import *
+from .nuclear_physics_utils import *
+from .hartree_fock_library import *
+from .qml_models import *
+from .qml_utils import *
+from .utils_quasiparticle_approximation import *
+from .sdg_utils import *
+
+__all__ = [
+    "hamiltonian_utils",
+    "cg_utils",
+    "fermi_hubbard_library",
+    "nuclear_physics_utils",
+    "hartree_fock_library",
+    "qml_models",
+    "qml_utils",
+    "utils_quasiparticle_approximation",
+    "sdg_utils",
+]
